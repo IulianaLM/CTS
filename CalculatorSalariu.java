964b84746668c9ce8cf83084331d94c86c0918a7
Litoiu Iuliana <164898082+IulianaLM@users.noreply.github.com>
2025-03-10 13:17:32 +0200

    Delete CalculatorSalariu.java

--- CalculatorSalariu.java
+++
@@ -1,9 +0,0 @@
-public class CalculatorSalariu {
-    public double calculatorSalariuLucrator(Lucrator lucrator){
-        return lucrator.getTarifOra()*lucrator.getNrOreLucrate();
-    }
-
-    public double calculSalariuManager(Manager manager){
-        return manager.getSalariuBaza() + manager.getNrProiecte() * 1000;
-    }
-}
